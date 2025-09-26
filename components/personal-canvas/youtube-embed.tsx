88cc221df14150769e9be10da3b387cc7c4ae2d3
drrhodium <rohithejjaparapu@gmail.com>
2025-09-26 12:35:24 +0530

    Remove autoplay from YouTube video - show thumbnail with manual play controls

--- components/personal-canvas/youtube-embed.tsx
+++ components/personal-canvas/youtube-embed.tsx
@@ -1,47 +1,14 @@
-'use client';
-import { useEffect, useRef } from 'react';
-
 interface YouTubeEmbedProps {
   videoId: string;
 }
 
 export function YouTubeEmbed({ videoId }: YouTubeEmbedProps) {
-  const containerRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    const observer = new IntersectionObserver(
-      (entries) => {
-        const iframe = containerRef.current?.querySelector('iframe');
-        if (!iframe || !iframe.contentWindow) {
-            return;
-        }
-        if (entries[0].isIntersecting) {
-          iframe.contentWindow.postMessage('{"event":"command","func":"playVideo","args":""}', '*');
-        } else {
-          iframe.contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');
-        }
-      },
-      { threshold: 0.7, rootMargin: '50px' } // Start playing when 70% visible, with 50px margin
-    );
-
-    const currentRef = containerRef.current;
-    if (currentRef) {
-      observer.observe(currentRef);
-    }
-
-    return () => {
-      if (currentRef) {
-        observer.unobserve(currentRef);
-      }
-    };
-  }, []);
-
   return (
-    <div ref={containerRef} className="w-full h-full">
+    <div className="w-full h-full">
       <iframe
         width="100%"
         height="100%"
-        src={`https://www.youtube.com/embed/${videoId}?enablejsapi=1&autoplay=0&mute=1&loop=1&playlist=${videoId}&controls=0&rel=0&modestbranding=1`}
+        src={`https://www.youtube.com/embed/${videoId}?autoplay=0&controls=1&rel=0&modestbranding=1`}
         title="YouTube video player"
         frameBorder="0"
         loading="lazy"
